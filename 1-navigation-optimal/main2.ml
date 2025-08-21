ff967332f9744c1649775372f3aba1afea4f9d95
Nigel Withrow <nigelwithrow78@gmail.com>
2025-08-22 00:15:47 +0530

    need finalization

--- 1-navigation-optimal/main2.ml
+++ 1-navigation-optimal/main2.ml
@@ -19,124 +19,111 @@
 
 (* Input data-types *)
 type rect = { x : int; y : int; width : int; height : int }
-
-let left { x; y; width; height } = ((x, y), (x, y + height))
-let right { x; y; width; height } = ((x + width, y), (x + width, y + height))
-
 type input = { goal : int; rects : rect list }
+type coord = { x : int; y : int }
+type output = { path : coord list; distance : float }
 
+(* Distance between two cartesian points *)
+let calc_dist (x, y) (x', y') =
+  let open Float in
+  let x, x', y, y' = (of_int x, of_int x', of_int y, of_int y') in
+  sqrt (add (pow (sub y' y) 2.) (pow (sub x' x) 2.))
+
+(* Graph stuff *)
 module Graph = struct
-  type edge = { pair : int * int; weight : float }
+  module G = struct
+    module IntInt = struct
+      type t = int * int
 
-  type 'a t = {
-    node_seq : 'a list;
-    node_exist : ('a, int) Hashtbl.t;
-    node_edges : (*edge id*) int list Array.t;
-    edge_seq : edge list;
-  }
+      let compare = Stdlib.compare
+      let equal = ( = )
+      let hash = Hashtbl.hash
+    end
 
-  let init node_seq =
-    let open Seq in
-    let count = length node_seq in
-    {
-      node_seq = List.of_seq node_seq;
-      node_exist = Hashtbl.of_seq @@ mapi (fun i node -> (node, i)) node_seq;
-      node_edges = Array.init count (fun _ -> []);
-      edge_seq = [];
-    }
+    module Float = struct
+      type t = float
 
-  let add_edge a b weight graph =
-    let open Option in
-    let open Hashtbl in
-    let fail =
-      Assert_failure
-        ("start & goal should be different nodes existing in the graph", 0, 0)
-    in
-    if a = b then raise fail;
-    match (find_opt graph.node_exist a, find_opt graph.node_exist b) with
-    | Some a_id, Some b_id ->
-        let edge = { pair = (a_id, b_id); weight } in
-        let edge_id = List.length graph.edge_seq in
-        let replace id =
-          graph.node_edges.(id) <- edge_id :: graph.node_edges.(id)
-        in
-        replace a_id;
-        replace b_id;
-        { graph with edge_seq = edge :: graph.edge_seq }
-    | _ -> raise fail
+      let compare = Stdlib.compare
+      let default = 0.
+    end
 
-  let neighbors node graph =
-    let open Option in
-    let open Hashtbl in
-    find_opt graph.node_exist node
-    |> map (fun node ->
-           graph.node_edges.(node) |> List.map (List.nth graph.edge_seq))
+    include Graph.Imperative.Graph.ConcreteLabeled (IntInt) (Float)
+  end
 
-  let dfs_min_lightest_path start goal graph =
-    let open Hashtbl in
-    let open Option in
-    match (find_opt graph.node_exist start, find_opt graph.node_exist goal) with
-    | Some start, Some goal ->
-        let visited =
-          Array.init (List.length graph.node_seq) (fun _ -> false)
-        in
-        let min_path = None in
-        let rec dfs_rec s path =
-          if s = goal then path else visited.(s) <- true;
-          graph.node_edges.(s)
-          |> List.map (fun neighbor ->
-                 if not visited.(neighbor) then dfs_rec neighbor path else [])
-        in
-        dfs_rec start []
-    | _ ->
-        raise
-        @@ Assert_failure
-             ( "start & goal should be different nodes existing in the graph",
-               0,
-               0 )
+  module Dijkstra =
+    Graph.Path.Dijkstra
+      (G)
+      (struct
+        type edge = G.E.t
+        type t = float
+
+        let weight (a, _, b) = calc_dist a b
+        let zero = 0.
+        let add = Float.add
+        let compare = compare
+      end)
+
+  let create = G.create
+  let add_vertex = G.add_vertex
+  let add_edge = G.add_edge
+  let shortest_path = Dijkstra.shortest_path
 end
 
 (* whether line between two points (x, y) & (x', y') intersects with rectange
    `rect` *)
 let intersect_line_rect (x, y) (x', y') (rect : rect) =
-  let open Float in
-  (* From `(x,y), (x',y')` line equation to `y = mx + c` equation *)
-  let mx_plus_c_of_2_point (x, y) (x', y') =
-    let m = (div @@ sub (of_int y') (of_int y)) @@ sub (of_int x') (of_int x) in
-    let c = sub (of_int y) (mul (of_int x) m) in
-    (m, c)
-  in
-
-  let rect_base_y = rect.y in
-  let rect_top_y = rect.y + rect.height in
-  let m, c = mx_plus_c_of_2_point (x, y) (x', y') in
-  if m = 0. then
-    (* line is paralell to x-axis *)
-    y >= rect_base_y && y <= rect_top_y
-  else if is_infinite m then
-    (* line is paralell to y-axis *)
-    x > rect.x && x < rect.x + rect.width
+  if
+    (* if line's x-bounds are completely out of rectangle's x-bounds, they can't
+       intersect *)
+    max x x' <= min rect.x (rect.x + rect.width)
+    || min x x' >= max rect.x (rect.x + rect.width)
+    (* if line's y-bounds are completely out of rectangle's y-bounds, they can't
+       intersect *)
+    || max y y' <= min rect.y (rect.y + rect.height)
+    || min y y' >= max rect.y (rect.y + rect.height)
+  then false
   else
-    (* mantissa of the line at the level of the base and top of the rectangle
-       respectively *)
-    let line_x_min, line_x_max =
-      min_max
-        (div (sub (of_int rect_base_y) c) m)
-        (div (sub (of_int rect_top_y) c) m)
+    let open Float in
+    (* From `(x,y), (x',y')` line equation to `y = mx + c` equation *)
+    let mx_plus_c_of_2_point (x, y) (x', y') =
+      let m =
+        (div @@ sub (of_int y') (of_int y)) @@ sub (of_int x') (of_int x)
+      in
+      let c = sub (of_int y) (mul (of_int x) m) in
+      (m, c)
     in
-    line_x_max > of_int rect.x && line_x_min < of_int (rect.x + rect.width)
+    let rect_base_y = rect.y in
+    let rect_top_y = rect.y + rect.height in
+    let m, c = mx_plus_c_of_2_point (x, y) (x', y') in
+    if m = 0. then
+      (* line is paralell to x-axis *)
+      y >= rect_base_y && y <= rect_top_y
+    else if is_infinite m then
+      (* line is paralell to y-axis *)
+      x > rect.x && x < rect.x + rect.width
+    else
+      (* mantissa of the line at the level of the base and top of the rectangle
+       respectively *)
+      let line_x_base = div (sub (of_int rect_base_y) c) m in
+      let line_x_top = div (sub (of_int rect_top_y) c) m in
+      (* line passes through rectangle's base edge *)
+      (line_x_base > of_int rect.x && line_x_base < of_int (rect.x + rect.width))
+      (* line passes through rectangle's top edge *)
+      || line_x_top > of_int rect.x
+         && line_x_top < of_int (rect.x + rect.width)
+      (* line entered rectangle at slant from bottom left to top right*)
+      || line_x_base <= of_int rect.x
+         && line_x_top >= of_int (rect.x + rect.width)
+      (* line entered rectangle at slant from bottom right to top left*)
+      || line_x_base >= of_int (rect.x + rect.width)
+         && line_x_top <= of_int rect.x
 
-(* Calculates the distance of the path represented by the given coordinates *)
-let rec calc_dist = function
-  | [] | [ _ ] -> 0.
-  | (x, y) :: ((x', y') as new_head) :: xs ->
-      let open Float in
-      let x, x', y, y' = (of_int x, of_int x', of_int y, of_int y') in
-      sqrt (add (pow (sub y' y) 2.) (pow (sub x' x) 2.))
-      |> add (calc_dist @@ (new_head :: xs))
-
-let graph_of_input (input : input) =
+(*
+  NOTE: Solution
+*)
+let solve (input : input) : output =
   let open List in
+  let open Graph in
   let rect_corners =
     input.rects
     |> map (fun { x; y; width; height } ->
@@ -144,7 +131,8 @@ let graph_of_input (input : input) =
     |> flatten
   in
   let nodes = [ (0, 0); (0, input.goal) ] @ rect_corners in
-  let graph = ref @@ Graph.init @@ List.to_seq nodes in
+  let graph = create () in
+  nodes |> iter (add_vertex graph);
   for i = 0 to length nodes - 1 do
     let a = nth nodes i in
     for j = i + 1 to length nodes - 1 do
@@ -157,38 +145,60 @@ let graph_of_input (input : input) =
                else false)
              true
       in
-      if valid_edge then graph := Graph.add_edge a b (calc_dist [ a; b ]) !graph
+      if valid_edge then add_edge graph a b
     done
   done;
-  (* !edges
-  |> iter (fun { nodes = (ax, ay), (bx, by); distance } ->
-         Printf.printf "(%d, %d) ---%f--> (%d, %d)\n" ax ay distance bx by); *)
-  graph
+  let path, _ = shortest_path graph (0, 0) (0, input.goal) in
+  {
+    path = { x = 0; y = 0 } :: (path |> map (fun (_, _, (x, y)) -> { x; y }));
+    distance =
+      path |> fold_left (fun sum (a, _, b) -> Float.add sum @@ calc_dist a b) 0.;
+  }
 
-let dfs_shortest_path (graph : (int * int) Graph.t) start goal =
-  let open Hashtbl in
-  let open Option in
-  if
-    (is_none @@ find_opt graph.nodes start)
-    || (is_none @@ find_opt graph.nodes goal)
-  then raise @@ Assert_failure ("start and goal should be in graph", 0, 0);
-  let dfs' _ = _ in
-  dfs' graph.edges
+(*
+  Solve the given input & generate the `desmos` expressions to visualize the
+  rectangles & the path taken
+*)
+let generate_desmos input =
+  let print_list draw label list =
+    let xh, xs =
+      match (List.to_seq list) () with
+      | Cons (first, rest) -> (first, rest)
+      | Nil -> raise Exit
+    in
+    Printf.printf "%s = [%s" label (draw xh);
+    Seq.iter (fun x -> Printf.printf ",%s" @@ draw x) xs;
+    print_endline "]"
+  in
+  let soln = solve input in
+  Printf.printf "# Desmos Output\n";
+  Printf.printf "# Distance: %f units\n" soln.distance;
+  (* Draw list of points in solution path *)
+  soln.path |> print_list (fun { x; y } -> Printf.sprintf "(%d,%d)" x y) "p";
+  (* Draw rectangles *)
+  input.rects
+  |> List.iteri (fun i (rect : rect) ->
+         let tag = string_of_int i in
+         let print_list = print_list string_of_int in
+         print_list
+           (Printf.sprintf "x_%s" tag)
+           [ rect.x; rect.x; rect.x + rect.width; rect.x + rect.width ];
+         print_list
+           (Printf.sprintf "y_%s" tag)
+           [ rect.y; rect.y + rect.height; rect.y + rect.height; rect.y ];
+         Printf.printf "polygon(x_%s, y_%s)\n" tag tag)
 ;;
 
-let graph =
-  graph_of_input
-    {
-      goal = 48;
-      rects =
-        [
-          { x = -18; y = 31; width = 22; height = 8 };
-          { x = -6; y = 10; width = 22; height = 8 };
-        ];
-    }
-in
-dfs_shortest_path graph
 (*
-  NOTE: Solution
+  NOTE: Sample Example
 *)
-(* let solve input : output = _ *)
+generate_desmos
+  {
+    goal = 48;
+    rects =
+      [
+        { x = -18; y = 31; width = 22; height = 8 };
+        { x = -6; y = 10; width = 102; height = 8 };
+        { x = -5; y = 20; width = 32; height = 8 };
+      ];
+  }
